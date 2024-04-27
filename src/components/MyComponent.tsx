9be3eebce2fd6f0795b72a649edaa5394f60d288
NW-DbINYA <ALgoRITm2608@yandex.ru>
2024-04-27 14:51:32 +0300

    add

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,14 @@
+export interface PersonalData {
+  name: string;
+  surname: string;
+}
+
+const MyComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default MyComponent;
